c8b4b4bc2551e8f3f5ff175b06aa4b0a3837d31c
Alexander Rey <mail@alexanderrey.ca>
2025-03-03 14:20:30 -0500

    Test Dev Auto Restart B2

--- API/responseLocal.py
+++ API/responseLocal.py
@@ -4091,7 +4091,7 @@ async def PW_Forecast(
         returnOBJ["flags"]["sourceTimes"] = sourceTimes
         returnOBJ["flags"]["nearest-station"] = int(0)
         returnOBJ["flags"]["units"] = unitSystem
-        returnOBJ["flags"]["version"] = "V2.5.4a"
+        returnOBJ["flags"]["version"] = "V2.5.4b"
 
         if version >= 2:
             returnOBJ["flags"]["sourceIDX"] = sourceIDX
