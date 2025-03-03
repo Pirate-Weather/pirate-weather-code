fa9838fcb4df7c15d00230313fe9b5c31cfb5073
Kev <cloneofghosts@users.noreply.github.com>
2025-03-03 15:11:15 -0500

    Test AWS push

--- API/responseLocal.py
+++ API/responseLocal.py
@@ -4091,7 +4091,7 @@ async def PW_Forecast(
         returnOBJ["flags"]["sourceTimes"] = sourceTimes
         returnOBJ["flags"]["nearest-station"] = int(0)
         returnOBJ["flags"]["units"] = unitSystem
-        returnOBJ["flags"]["version"] = "V2.5.4b"
+        returnOBJ["flags"]["version"] = "V2.5.4c"
 
         if version >= 2:
             returnOBJ["flags"]["sourceIDX"] = sourceIDX
