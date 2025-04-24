c83e9acd409b29f23d02bb22711bb82304bdb3f0
cloneofghosts <10248058+cloneofghosts@users.noreply.github.com>
2025-04-24 14:13:21 +0000

    style fixes by ruff

--- API/PirateText.py
+++ API/PirateText.py
@@ -64,7 +64,7 @@ def calculate_text(
     # If type is current precipitation probability should always be 1 otherwise if it exists in the hourObject use it otherwise use 1
     if type == "current":
         pop = 1
-    elif "precipProbability" in hourObject or  hourObject["precipProbability"] != -999:
+    elif "precipProbability" in hourObject or hourObject["precipProbability"] != -999:
         pop = hourObject["precipProbability"]
     else:
         pop = 1
