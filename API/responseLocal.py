6a19e0c04cdb984d9d53bd1702f63c3d48c13a41
Alexander Rey <mail@alexanderrey.ca>
2025-06-09 11:41:58 -0400

    Update version number for prod patch

--- API/responseLocal.py
+++ API/responseLocal.py
@@ -4205,7 +4205,7 @@ async def PW_Forecast(
         returnOBJ["flags"]["sourceTimes"] = sourceTimes
         returnOBJ["flags"]["nearest-station"] = int(0)
         returnOBJ["flags"]["units"] = unitSystem
-        returnOBJ["flags"]["version"] = "V2.7.0"
+        returnOBJ["flags"]["version"] = "V2.7.1"
         if version >= 2:
             returnOBJ["flags"]["sourceIDX"] = sourceIDX
             returnOBJ["flags"]["processTime"] = (
