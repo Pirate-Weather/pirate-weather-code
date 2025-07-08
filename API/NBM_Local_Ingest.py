121e554a08c46280f51e6068fda656e188b123e3
alexander0042 <26213938+alexander0042@users.noreply.github.com>
2025-07-08 19:41:13 +0000

    style fixes by ruff

--- API/NBM_Local_Ingest.py
+++ API/NBM_Local_Ingest.py
@@ -1075,9 +1075,7 @@ if saveType == "S3":
     )
 
     # Write most recent forecast time
-    with open(
-        forecast_process_dir + "/NBM.time.pickle", "wb"
-    ) as file:
+    with open(forecast_process_dir + "/NBM.time.pickle", "wb") as file:
         # Serialize and write the variable to the file
         pickle.dump(base_time, file)
 
