e57bcbb08b02368720b33fd2632e73f4e037623e
Alexander Rey <mail@alexanderrey.ca>
2025-07-08 10:49:20 -0400

    Better reshape logic

--- API/GFS_Local_Ingest.py
+++ API/GFS_Local_Ingest.py
@@ -850,7 +850,7 @@ for daskVarIDX, dask_var in enumerate(zarrVars[:]):
     if dask_var == "time":
         # Create a time array with the same shape
         daskVarArraysShape = da.reshape(
-            daskVarArraysStack, (hisPeriod, 1), merge_chunks=False
+            daskVarArraysStack, (daskVarArraysStack.shape[0] * daskVarArraysStack.shape[1], 1), merge_chunks=False
         )
         daskCatTimes = da.concatenate(
             (da.squeeze(daskVarArraysShape), daskForecastArray), axis=0
@@ -870,7 +870,7 @@ for daskVarIDX, dask_var in enumerate(zarrVars[:]):
 
     else:
         daskVarArraysShape = da.reshape(
-            daskVarArraysStack, (hisPeriod, 721, 1440), merge_chunks=False
+            daskVarArraysStack, (daskVarArraysStack.shape[0] * daskVarArraysStack.shape[1], 721, 1440), merge_chunks=False
         )
         daskArrayOut = da.concatenate((daskVarArraysShape, daskForecastArray), axis=0)
 
