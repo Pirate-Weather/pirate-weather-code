1075bf4b5e21344cabffb8e5ff47d57d11c42ed4
Kev <cloneofghosts@users.noreply.github.com>
2025-07-17 12:39:05 -0400

    Update TimeMachine version

--- API/timemachine.py
+++ API/timemachine.py
@@ -980,7 +980,7 @@ async def TimeMachine(
         returnOBJ["flags"]["sources"] = "ERA5"
         returnOBJ["flags"]["nearest-station"] = int(0)
         returnOBJ["flags"]["units"] = unitSystem
-        returnOBJ["flags"]["version"] = "V2.7.3"
+        returnOBJ["flags"]["version"] = "V2.7.4"
         returnOBJ["flags"]["sourceIDX"] = {"x": y, "y": x}
         returnOBJ["flags"]["processTime"] = (
             datetime.datetime.utcnow() - T_Start
