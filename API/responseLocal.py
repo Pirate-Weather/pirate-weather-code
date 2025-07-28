1d67db43d7c5bbe4f3465aeae6cb9953760b88ef
Kev <cloneofghosts@users.noreply.github.com>
2025-07-28 18:23:52 -0400

    Change version to b

--- API/responseLocal.py
+++ API/responseLocal.py
@@ -4353,7 +4353,7 @@ async def PW_Forecast(
         returnOBJ["flags"]["sourceTimes"] = sourceTimes
         returnOBJ["flags"]["nearest-station"] = int(0)
         returnOBJ["flags"]["units"] = unitSystem
-        returnOBJ["flags"]["version"] = "V2.7.5a"
+        returnOBJ["flags"]["version"] = "V2.7.5b"
         if version >= 2:
             returnOBJ["flags"]["sourceIDX"] = sourceIDX
             returnOBJ["flags"]["processTime"] = (
