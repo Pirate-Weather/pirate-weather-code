9549340b73d1e61070d6bba0b01b4656c778423e
Kev <cloneofghosts@users.noreply.github.com>
2025-09-08 10:13:10 -0400

    Change a for snow to 600 from 200

--- API/responseLocal.py
+++ API/responseLocal.py
@@ -1004,6 +1004,7 @@ def dbz_to_rate(dbz_array, precip_type_array, min_dbz=5.0):
 
     # Apply 'snow' coefficients where precip_type is 'snow'
     snow_mask = precip_type_array == "snow"
+    a_array[snow_mask] = 600.0
     b_array[snow_mask] = 2.0
 
     # Compute precipitation rate
