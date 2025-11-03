bca816887d0242630b78e69c17b50b5c7538a7c4
Alexander Rey <mail@alexanderrey.ca>
2025-11-03 21:49:52 +0000

    Version Bump

--- API/constants/api_const.py
+++ API/constants/api_const.py
@@ -81,7 +81,7 @@ PRECIP_IDX = {
 
 # API versioning and ingest version constants
 # Version scheme is: Major.Minor.Patch
-API_VERSION = "V2.8f"
+API_VERSION = "V2.8g"
 
 # Command priorities
 NICE_PRIORITY = 20
