bf860c33b920341988c67a43fe42ab1a4eb0998c
cloneofghosts <thekpv8@gmail.com>
2025-11-03 10:01:06 -0500

    Change Houston to Ottawa

--- tests/test_compare_production.py
+++ tests/test_compare_production.py
@@ -96,8 +96,8 @@ def _diff_nested(a: object, b: object, path: str = "") -> dict:
 def test_local_vs_production():
     client = _get_client()
 
-    # Houston, TX (29.7604, -95.3698) and London, UK (51.50853, -0.12574)
-    for lat, lon in [(29.7604, -95.3698), (51.50853, -0.12574)]:
+    # Ottawa, ON (45.4215, -75.6972) and London, UK (51.50853, -0.12574)
+    for lat, lon in [(45.4215, -75.6972), (51.50853, -0.12574)]:
         local_resp = client.get(f"/forecast/{PW_API}/{lat},{lon}?version=2")
         assert local_resp.status_code == 200
         local_data = local_resp.json()
