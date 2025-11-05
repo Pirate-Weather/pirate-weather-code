bc1a1a09995ac9ef0b2f9b1d231e0b6ca61f2c57
alexander0042 <26213938+alexander0042@users.noreply.github.com>
2025-11-05 15:21:41 +0000

    style fixes by ruff

--- API/responseLocal.py
+++ API/responseLocal.py
@@ -521,7 +521,6 @@ def update_zarr_store(initialRun):
                 command = f"nice -n 20 rm -rf /tmp/{old_dir}"
                 subprocess.run(command, shell=True)
 
-
     print("Refreshed Zarrs")
 
 
