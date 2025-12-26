e804946f564640fa2d54b41e375cb3269c149f41
cloneofghosts <thekpv8@gmail.com>
2025-12-26 14:06:56 -0500

    [skip ci] Update version number

--- API/constants/api_const.py
+++ API/constants/api_const.py
@@ -93,7 +93,7 @@ PRECIP_ACCUM_NOISE_THRESHOLD = (
 
 # API versioning and ingest version constants
 # Version scheme is: Major.Minor.Patch
-API_VERSION = "V2.9.0"
+API_VERSION = "V2.9.1a"
 
 # Generic API constants
 MAX_S3_RETRIES = 5
