24d5b3de2ea274c3d62888ec649011c13c468960
cloneofghosts <thekpv8@gmail.com>
2026-03-16 17:36:40 -0400

    Change to 1km reflectivity instead of entire atmosphere

--- API/HRRR_Local_Ingest.py
+++ API/HRRR_Local_Ingest.py
@@ -157,7 +157,7 @@ zarr_vars = (
     "CRAIN_surface",
     "TCDC_entireatmosphere",
     "MASSDEN_8maboveground",
-    "REFC_entireatmosphere",
+    "REFD_1000maboveground",
     "DSWRF_surface",
     "CAPE_surface",
 )
@@ -173,10 +173,11 @@ matchstring_8m = ":(MASSDEN:8 m above ground:)"
 matchstring_su = (
     ":((CRAIN|CICEP|CSNOW|CFRZR|PRATE|VIS|GUST|DSWRF|CAPE):surface:.*hour fcst)"
 )
+matchstring_1000m = "(:REFD:1000 m above ground:)"
 matchstring_10m = "(:(UGRD|VGRD):10 m above ground:.*hour fcst)"
 matchstring_cl = "(:TCDC:entire atmosphere:.*hour fcst)"
 matchstring_ap = "(:APCP:surface:0-[1-9]*)"
-matchstring_sl = "(:(MSLMA|REFC):)"
+matchstring_sl = "(:(MSLMA):)"
 
 # Merge matchstrings for download
 match_strings = (
@@ -185,6 +186,8 @@ match_strings = (
     + matchstring_su
     + "|"
     + matchstring_10m
+    + "|"
+    + matchstring_1000m
     + "|"
     + matchstring_cl
     + "|"
@@ -326,9 +329,9 @@ xarray_forecast_merged["MASSDEN_8maboveground"] = (
 )
 
 
-# Set REFC values < 5 to 0
-xarray_forecast_merged["REFC_entireatmosphere"] = mask_invalid_refc(
-    xarray_forecast_merged["REFC_entireatmosphere"]
+# Set REFD values < 5 to 0
+xarray_forecast_merged["REFD_1000maboveground"] = mask_invalid_refd(
+    xarray_forecast_merged["REFD_1000maboveground"]
 )
 
 # %% Save merged and processed xarray dataset to disk using zarr with compression
@@ -705,7 +708,7 @@ close_store(zarr_store)
 # 9 (PRATE)
 # 11:14 (PTYPE)
 # 16 (MASSDEN)
-# 17 (REFC)
+# 17 (REFD)
 
 # Add padding for map chunking (100x100)
 daskVarArrayStackDisk_maps = pad_to_chunk_size(daskVarArrayStackDisk, 100)
