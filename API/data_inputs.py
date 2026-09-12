53b2980abd5d1fc0308c36284fc3e4b1cd19b351
Kev <cloneofghosts@users.noreply.github.com>
2026-09-12 15:54:42 -0400

    fix: Use precipitation priority for accumulation (#736)
    Co-authored-by: cloneofghosts <10248058+cloneofghosts@users.noreply.github.com>

--- API/data_inputs.py
+++ API/data_inputs.py
@@ -951,12 +951,12 @@ def prepare_data_inputs(
     )
 
     # --- accum_inputs ---
-    accum_inputs = _stack_with_priority(
+    accum_inputs = _stack_precip_with_priority(
         num_hours,
         lat,
         lon,
         source_data={
-            "nbm": nbm_merged[:, NBM["intensity"]] if nbm_merged is not None else None,
+            "nbm": nbm_merged[:, NBM["accum"]] if nbm_merged is not None else None,
             "hrrr": hrrr_merged[:, HRRR["accum"]] if hrrr_merged is not None else None,
             "hrdps": hrdps_merged[:, HRDPS["accum"]]
             if hrdps_merged is not None
